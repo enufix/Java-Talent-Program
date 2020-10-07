209bbfdf6eb8499d77e4c69befcf4c31a6c339a2
Elena Dimanoska <dimanoskaelena@gmail.com>
2020-10-07 20:09:52 +0200

    Notes-frontend

---
+++ Day09/notes-react/src/components/Nav.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+class Nav extends Component {
+  render() {
+    const { toggleNote, showNote } = this.props;
+    
+    return (
+      <div className="nav-container">
+        <div className="nav-logo">Seavus Notes</div>
+        <div className="nav-button" onClick={() => toggleNote()} >
+          { showNote ? 'Cancel' :  '+ Note' }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Nav;
